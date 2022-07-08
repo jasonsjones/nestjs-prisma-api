549a2bdf67bcdd9c1249fc09f5cc9290ab617e70
Jason Jones <jsjones96@gmail.com>
2022-07-08 08:34:44 -0700

    feat: add prisma module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,9 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, swaggerConfig);
     SwaggerModule.setup('api', app, document);
+
+    const prismaService = app.get(PrismaService);
+    await prismaService.enableShutdownHooks(app);
 
     await app.listen(3000);
 }
