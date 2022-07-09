4e423f8e0daeb7dc02b9c1924d8eaff265b50f65
Jason Jones <jsjones96@gmail.com>
2022-07-09 12:51:02 -0700

    test: add project config to use test db

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaTestService } from './prisma.test-service';
+
+const prismaServiceProvider = {
+    provide: PrismaService,
+    useClass:
+        process.env.NODE_ENV !== 'test' ? PrismaService : PrismaTestService
+};
 
 @Module({
-    providers: [PrismaService],
+    providers: [prismaServiceProvider],
     exports: [PrismaService]
 })
 export class PrismaModule {}
